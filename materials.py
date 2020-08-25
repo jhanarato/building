2f27605be1f366b628a4229fa4f2bc7be6920f8b
Jhanarato <jhanarato@gmail.com>
2020-08-25 17:59:14 +0800

    Data entry done. Can write out the materials used for each section in csv format.

--- materials.py
+++ materials.py
@@ -1,7 +1,9 @@
 # A script to calculate building material requirements for the stables project
 
+import csv
+
 # Insulation products
-sound_shield_70 = {"name" : "Bradford SoundShield Wall Batts 70mm",
+sound_screen_70 = {"name" : "Bradford SoundScreen Wall Batts 70mm",
                "thickness" : 70,
                "length" : 1160,
                "width" : 580,
@@ -11,7 +13,7 @@ sound_shield_70 = {"name" : "Bradford SoundShield Wall Batts 70mm",
                "pack-coverage" : 6.8,
                "product-code" : 182313 }
 
-sound_shield_88 = {"name" : "Bradford SoundShield Wall Batts 88mm",
+sound_screen_88 = {"name" : "Bradford SoundScreen Wall Batts 88mm",
                "thickness" : 88,
                "length" : 1160,
                "width" : 580,
@@ -56,4 +58,56 @@ ne_bedroom_ceiling = {"name" : "Northeast Bedroom, Ceiling",  "area" : 14.0 }
 se_bedroom_ceiling = {"name" : "Southeast Bedroom, Ceiling",  "area" : 14.0 }
 sw_bedroom_ceiling = {"name" : "Southwest Bedroom, Ceiling",  "area" : 14.0 }
 nw_bedroom_ceiling = {"name" : "Northwest Bedroom, Ceiling",  "area" : 14.0 }
+
+living_e = {"name" : "Living Area, East Wall, Adjacent to Bedroom", "area" : 17.6}
+living_s = {"name" : "Living Area, South Wall", "area" : 20.6}
+living_w = {"name" : "Living Area, West Wall, Main Entrance", "area" : 9.2}
+living_ceiling = {"name" : "Living Area, Raked Ceiling", "area" : 60.5}
+
+laundry_partition = {"name" : "Toilets and Laundry Partition Walls", "area" : 11.5}
+laundry_ceiliing = {"name" : "Toilets and Laundry Ceiling", "area" : 7.0}
+
+showers_partition = {"name" : "Showers Partition Walls", "area" : 7.5}
+showers_ceiliing = {"name" : "Showers Ceiling", "area" : 8.1}
+
+disabled_wall = {"name" : "Disabled bathroom wall", "area" : 6.9}
+disabled_ceiling = {"name" : "Disabled bathroom ceiling", "area" : 9.6}
+
+# A list of tuples matching insulation product to section to insulate.
+product_section_match = [(ne_bedroom_n, sound_screen_88),
+                         (ne_bedroom_e, sound_screen_88),
+                         (ne_bedroom_s, sound_screen_70),
+                         (ne_bedroom_w, sound_screen_70),
+                         (se_bedroom_n, sound_screen_70),
+                         (se_bedroom_e, sound_screen_88),
+                         (se_bedroom_s, sound_screen_88),
+                         (se_bedroom_w, sound_screen_70),
+                         (sw_bedroom_n, sound_screen_70),
+                         (sw_bedroom_e, sound_screen_70),
+                         (sw_bedroom_s, sound_screen_88),
+                         (sw_bedroom_w, sound_screen_70),
+                         (nw_bedroom_n, sound_screen_88),
+                         (nw_bedroom_e, sound_screen_70),
+                         (nw_bedroom_s, sound_screen_70),
+                         (nw_bedroom_w, sound_screen_70),
+                         (ne_bedroom_ceiling, sound_screen_88),
+                         (se_bedroom_ceiling, sound_screen_88),
+                         (se_bedroom_ceiling, sound_screen_88),
+                         (se_bedroom_ceiling, sound_screen_88),
+                         (living_e, sound_screen_88),
+                         (living_s, sound_screen_88),
+                         (living_w, sound_screen_88),
+                         (living_ceiling, gold_hp_240),
+                         (laundry_partition, sound_screen_88),
+                         (laundry_ceiliing, gold_hp_240),
+                         (showers_partition, sound_screen_88),
+                         (showers_ceiliing, gold_hp_240),
+                         (disabled_wall, sound_screen_88),
+                         (disabled_ceiling, gold_hp_240)]
+
+
+with open("materials.csv", 'w',  newline='') as csvfile:
+        match_writer = csv.writer(csvfile)
+        for match in product_section_match:
+            match_writer.writerow([match[0]["name"], match[1]["name"]])
 
