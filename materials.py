d0d8fb74cf8c56891f5852fece740b1b411359ae
Jhanarato <jhanarato@gmail.com>
2020-08-25 16:47:31 +0800

    Prettied up a bit & added bedroom ceilings.

--- materials.py
+++ materials.py
@@ -32,39 +32,28 @@ gold_hp_240 = {"name" : "Bradford Gold High-Performance Ceiling Batts 240mm",
                "product-code" : 105419 }
 
 # Sections to insulate
-ne_bedroom_n = {"name" : "Northeast Bedroom, North Wall",
-                "area" : 5.8 }
-ne_bedroom_e = {"name" : "Northeast Bedroom, East Wall",
-                "area" : 7.9}
-ne_bedroom_s = {"name" : "Northeast Bedroom, South Wall",
-                "area" : 7.8}
-ne_bedroom_w = {"name" : "Northeast Bedroom, West Wall",
-                "area" : 11.0}
+ne_bedroom_n = {"name" : "Northeast Bedroom, North Wall", "area" : 5.8 }
+ne_bedroom_e = {"name" : "Northeast Bedroom, East Wall", "area" : 7.9}
+ne_bedroom_s = {"name" : "Northeast Bedroom, South Wall", "area" : 7.8}
+ne_bedroom_w = {"name" : "Northeast Bedroom, West Wall", "area" : 11.0}
 
-se_bedroom_n = {"name" : "Southeast Bedroom, North Wall",
-                "area" : 7.8 }
-se_bedroom_e = {"name" : "Southeast Bedroom, East Wall",
-                "area" : 7.9}
-se_bedroom_s = {"name" : "Southeast Bedroom, South Wall",
-                "area" : 5.8}
-se_bedroom_w = {"name" : "Southeast Bedroom, West Wall",
-                "area" : 11.0}
+se_bedroom_n = {"name" : "Southeast Bedroom, North Wall", "area" : 7.8 }
+se_bedroom_e = {"name" : "Southeast Bedroom, East Wall", "area" : 7.9}
+se_bedroom_s = {"name" : "Southeast Bedroom, South Wall", "area" : 5.8}
+se_bedroom_w = {"name" : "Southeast Bedroom, West Wall", "area" : 11.0}
 
-sw_bedroom_n = {"name" : "Southwest Bedroom, North Wall",
-                "area" : 7.8 }
-sw_bedroom_e = {"name" : "Southwest Bedroom, East Wall",
-                "area" : 11.0}
-sw_bedroom_s = {"name" : "Southwest Bedroom, South Wall",
-                "area" : 4.1}
-sw_bedroom_w = {"name" : "Southwest Bedroom, West Wall",
-                "area" : 11.0}
+sw_bedroom_n = {"name" : "Southwest Bedroom, North Wall", "area" : 7.8 }
+sw_bedroom_e = {"name" : "Southwest Bedroom, East Wall", "area" : 11.0}
+sw_bedroom_s = {"name" : "Southwest Bedroom, South Wall", "area" : 4.1}
+sw_bedroom_w = {"name" : "Southwest Bedroom, West Wall", "area" : 11.0}
 
-nw_bedroom_n = {"name" : "Northwest Bedroom, North Wall",
-                "area" :  2.8}
-nw_bedroom_e = {"name" : "Northwest Bedroom, East Wall",
-                "area" : 11.0}
-nw_bedroom_s = {"name" : "Northwest Bedroom, South Wall",
-                "area" : 4.1}
-nw_bedroom_w = {"name" : "Northwest Bedroom, West Wall",
-                "area" : 8.2}
+nw_bedroom_n = {"name" : "Northwest Bedroom, North Wall", "area" :  2.8}
+nw_bedroom_e = {"name" : "Northwest Bedroom, East Wall", "area" : 11.0}
+nw_bedroom_s = {"name" : "Northwest Bedroom, South Wall", "area" : 4.1};
+nw_bedroom_w = {"name" : "Northwest Bedroom, West Wall", "area" : 8.2}
+
+ne_bedroom_ceiling = {"name" : "Northeast Bedroom, Ceiling",  "area" : 14.0 }
+se_bedroom_ceiling = {"name" : "Southeast Bedroom, Ceiling",  "area" : 14.0 }
+sw_bedroom_ceiling = {"name" : "Southwest Bedroom, Ceiling",  "area" : 14.0 }
+nw_bedroom_ceiling = {"name" : "Northwest Bedroom, Ceiling",  "area" : 14.0 }
 
