c80f9d6439f3a42ecd9edf5bbe58ce4a90f50c47
Jhanarato <jhanarato@gmail.com>
2020-08-27 07:53:02 +0800

    Different configs supported now.

---
+++ config_1.py
@@ -0,0 +1,46 @@
+import sections, insulation
+
+# A list of tuples matching insulation product to section to insulate.
+product_section_match = [(sections.ne_bedroom_n, insulation.sound_screen_88),
+                         (sections.ne_bedroom_e, insulation.sound_screen_88),
+                         (sections.ne_bedroom_s, insulation.sound_screen_70),
+                         (sections.ne_bedroom_w, insulation.sound_screen_70),
+
+                         (sections.se_bedroom_n, insulation.sound_screen_70),
+                         (sections.se_bedroom_e, insulation.sound_screen_88),
+                         (sections.se_bedroom_s, insulation.sound_screen_88),
+                         (sections.se_bedroom_w, insulation.sound_screen_70),
+
+                         (sections.sw_bedroom_n, insulation.sound_screen_70),
+                         (sections.sw_bedroom_e, insulation.sound_screen_70),
+                         (sections.sw_bedroom_s, insulation.sound_screen_88),
+                         (sections.sw_bedroom_w, insulation.sound_screen_70),
+
+                         (sections.nw_bedroom_n, insulation.sound_screen_88),
+                         (sections.nw_bedroom_e, insulation.sound_screen_70),
+                         (sections.nw_bedroom_s, insulation.sound_screen_70),
+                         (sections.nw_bedroom_w, insulation.sound_screen_70),
+
+                         (sections.nw_bedroom_above_ceiling, insulation.gold_hp_wall_90),
+                         (sections.ne_bedroom_above_ceiling, insulation.gold_hp_wall_90),
+                         (sections.se_bedroom_above_ceiling, insulation.gold_hp_wall_90),
+                         (sections.sw_bedroom_above_ceiling, insulation.gold_hp_wall_90),
+
+                         (sections.nw_bedroom_ceiling, insulation.gold_hp_wall_90),
+                         (sections.ne_bedroom_ceiling, insulation.gold_hp_wall_90),
+                         (sections.se_bedroom_ceiling, insulation.gold_hp_wall_90),
+                         (sections.sw_bedroom_ceiling, insulation.gold_hp_wall_90),
+
+                         (sections.living_e, insulation.sound_screen_88),
+                         (sections.living_s, insulation.gold_hp_wall_90),
+                         (sections.living_w, insulation.gold_hp_wall_90),
+                         (sections.living_ceiling, insulation.gold_hp_ceiling_240),
+
+                         (sections.laundry_partition, insulation.sound_screen_88),
+                         (sections.laundry_ceiliing, insulation.gold_hp_ceiling_240),
+
+                         (sections.showers_partition, insulation.sound_screen_88),
+                         (sections.showers_ceiliing, insulation.gold_hp_ceiling_240),
+
+                         (sections.disabled_wall, insulation.gold_hp_wall_90),
+                         (sections.disabled_ceiling, insulation.gold_hp_ceiling_240)]
--- materials.py
+++ materials.py
@@ -2,57 +2,11 @@
 
 import csv
 
-import insulation, sections
+import insulation, config_1
 
 products = insulation.products
 products_by_code = [(product["code"],product) for product in products]
-
-
-# A list of tuples matching insulation product to section to insulate.
-product_section_match = [(sections.ne_bedroom_n, insulation.sound_screen_88),
-                         (sections.ne_bedroom_e, insulation.sound_screen_88),
-                         (sections.ne_bedroom_s, insulation.sound_screen_70),
-                         (sections.ne_bedroom_w, insulation.sound_screen_70),
-
-                         (sections.se_bedroom_n, insulation.sound_screen_70),
-                         (sections.se_bedroom_e, insulation.sound_screen_88),
-                         (sections.se_bedroom_s, insulation.sound_screen_88),
-                         (sections.se_bedroom_w, insulation.sound_screen_70),
-
-                         (sections.sw_bedroom_n, insulation.sound_screen_70),
-                         (sections.sw_bedroom_e, insulation.sound_screen_70),
-                         (sections.sw_bedroom_s, insulation.sound_screen_88),
-                         (sections.sw_bedroom_w, insulation.sound_screen_70),
-
-                         (sections.nw_bedroom_n, insulation.sound_screen_88),
-                         (sections.nw_bedroom_e, insulation.sound_screen_70),
-                         (sections.nw_bedroom_s, insulation.sound_screen_70),
-                         (sections.nw_bedroom_w, insulation.sound_screen_70),
-
-                         (sections.nw_bedroom_above_ceiling, insulation.gold_hp_wall_90),
-                         (sections.ne_bedroom_above_ceiling, insulation.gold_hp_wall_90),
-                         (sections.se_bedroom_above_ceiling, insulation.gold_hp_wall_90),
-                         (sections.sw_bedroom_above_ceiling, insulation.gold_hp_wall_90),
-
-                         (sections.nw_bedroom_ceiling, insulation.gold_hp_wall_90),
-                         (sections.ne_bedroom_ceiling, insulation.gold_hp_wall_90),
-                         (sections.se_bedroom_ceiling, insulation.gold_hp_wall_90),
-                         (sections.sw_bedroom_ceiling, insulation.gold_hp_wall_90),
-
-                         (sections.living_e, insulation.sound_screen_88),
-                         (sections.living_s, insulation.gold_hp_wall_90),
-                         (sections.living_w, insulation.gold_hp_wall_90),
-                         (sections.living_ceiling, insulation.gold_hp_ceiling_240),
-
-                         (sections.laundry_partition, insulation.sound_screen_88),
-                         (sections.laundry_ceiliing, insulation.gold_hp_ceiling_240),
-
-                         (sections.showers_partition, insulation.sound_screen_88),
-                         (sections.showers_ceiliing, insulation.gold_hp_ceiling_240),
-
-                         (sections.disabled_wall, insulation.gold_hp_wall_90),
-                         (sections.disabled_ceiling, insulation.gold_hp_ceiling_240)]
-
+product_section_match = config_1.product_section_match
 
 # Get a dictionary with the key being the product code
 # and the value being the total area of that product.
