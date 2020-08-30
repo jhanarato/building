572de0895c2c2cad0163a87b6d7a568f4d72c46a
Jhanarato <jhanarato@gmail.com>
2020-08-30 09:32:32 +0800

    Removed bathroom partitions.

--- p_config_1.py
+++ p_config_1.py
@@ -35,10 +35,10 @@ config = [(sections.ne_bedroom_n, plasterboard.gtek_fire_2400_by_1200),
           (sections.living_w, plasterboard.gtek_standard_2400_by_1200),
           (sections.living_ceiling, plasterboard.gtek_standard_2400_by_1200),
 
-          (sections.laundry_partition, plasterboard.gtek_fire_2400_by_1200),
+          #(sections.laundry_partition, plasterboard.gtek_fire_2400_by_1200),
           (sections.laundry_ceiliing, plasterboard.gtek_standard_2400_by_1200),
 
-          (sections.showers_partition, plasterboard.gtek_fire_2400_by_1200),
+          #(sections.showers_partition, plasterboard.gtek_fire_2400_by_1200),
           (sections.showers_ceiliing, plasterboard.gtek_standard_2400_by_1200),
 
           (sections.disabled_wall, plasterboard.gtek_fire_2400_by_1200),
